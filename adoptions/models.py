b62bff55b1187f297ba1a475e6c11e60b48fe387
Miguel Martinez <miguel.martinez@utexas.edu>
2018-02-25 00:41:43 -0500

    Fix models

--- adoptions/models.py
+++ adoptions/models.py
@@ -2,16 +2,15 @@ from django.db import models
 
 class Pet(models.Model):
     SEX_CHOICES = [('M', 'Male'), ('F', 'Female')]
-
-    name models.CharField(max_length=100)
-    submitter models.CharField(max_length=100)
-    species models.CharField(max_length=30)
-    breed models.CharField(max_length=30, black=True)
-    description models.TextField()
-    sex models.CharField(choices=SEX_CHOICES, max_length=1, blank=True)
+    name = models.CharField(max_length=100)
+    submitter = models.CharField(max_length=100)
+    species = models.CharField(max_length=30)
+    breed = models.CharField(max_length=30, blank=True)
+    description = models.TextField()
+    sex = models.CharField(choices=SEX_CHOICES, max_length=1, blank=True)
     submission_date = models.DateTimeField()
     age = models.IntegerField(null=True)
     vaccinations = models.ManyToManyField('Vaccine', blank=True)
 
 class Vaccine(models.Model):
-    name = model.CharField(max_length=50)
+    name = models.CharField(max_length=50)
